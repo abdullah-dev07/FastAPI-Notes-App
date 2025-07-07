b454990feb0f4d669bfa0b4f3cb44ef9f7d7dda2
Muhammad Abdullah <abdullah.muhammad@devsinc.com>
2025-07-08 01:22:48 +0500

    Initial commit

---
+++ config/db.py
@@ -0,0 +1,5 @@
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://abdullahmuhammad:Jac91785@fastapi-project.rmxvtac.mongodb.net/"
+
+conn= MongoClient(MONGO_URI)
---
+++ pydantictest.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class User(BaseModel):
+    id : int
+    name: str = "Abdullah Muhammad"
+    signup_ts : datetime | None = None
+    friends: list[int] = []
+
+
+external_data = {
+    "id": "123",
+    "signup_ts": "2017-06-01 12:22",
+    "friends": [1, "2", b"3"],
+
+}    
+
+user = User(**external_data)
+
+print(user.id)
